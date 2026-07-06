86ab1cba114f36021bff7d3c7098d48e48861cb0
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-07-06 18:33:54 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,7 @@ plugins {
 android {
     namespace = "app.flutter.picorigin"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
-
+    ndkVersion = flutter.ndkVersion
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -25,25 +24,14 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            //include("arm64-v8a")
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-            isUniversalApk = false
-        }
-    }
+
     buildTypes {
         getByName("release") {
-            // 使用 debug 签名配置（用于调试发布）
             signingConfig = signingConfigs.getByName("debug")
-
-            // 禁用 R8 混淆和资源压缩
             isMinifyEnabled = false // 禁用代码混淆
             isShrinkResources = false // 禁用资源压缩
         }
